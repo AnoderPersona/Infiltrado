618df8230e63d527dc39a70d85efe24aa989681a
AnoderPersona <tamibadilla@gmail.com>
2022-05-24 16:22:33 -0400

    monedas

--- main.py
+++ main.py
@@ -21,23 +21,27 @@ def despliega_matriz(alto_cuadrado, ancho_cuadrado, matrix):
 
             #pared
             if item == 0:
-                crea_cuadrado(x, y, (255, 255, 255), alto_cuadrado, ancho_cuadrado)
+                crea_cuadrado(x, y, (255, 255, 255), alto_cuadrado, ancho_cuadrado) #blanco
             #pieza
             elif item == 1:
-                crea_cuadrado(x, y, (200, 100, 60), alto_cuadrado, ancho_cuadrado)
-            #pasillo
+                crea_cuadrado(x, y, (200, 100, 60), alto_cuadrado, ancho_cuadrado) #naranjo
+            #pasillo o ex-moneda
             elif item == 2:
-                crea_cuadrado(x, y, (100, 200, 50), alto_cuadrado, ancho_cuadrado)
+                crea_cuadrado(x, y, (100, 200, 50), alto_cuadrado, ancho_cuadrado) #verde
             #obstaculo
-            else:
-                crea_cuadrado(x, y, (200, 50, 100), alto_cuadrado, ancho_cuadrado)
+            elif item == -1:
+                crea_cuadrado(x, y, (200, 50, 100), alto_cuadrado, ancho_cuadrado) #rosado oscuro
+            #moneda
+            elif item == -2:
+                crea_cuadrado(x, y, (200, 200, 20), alto_cuadrado, ancho_cuadrado) #verde amarillo
+
 
             x += ancho_cuadrado # for ever item/number in that row we move one "step" to the right
         y += alto_cuadrado   # for every new row we move one "step" downwards
     pygame.display.update()
 
 #================================================================
-#                       Obstaculos
+#                       Generacion random
 #================================================================
 
 def agrega_obstaculos(mat, n):
@@ -47,11 +51,26 @@ def agrega_obstaculos(mat, n):
         x = random.randrange(0,len(mat))
         y = random.randrange(0,len(mat[0]))
 
-        while (mat[x][y] < 1 or mat[x][y] == 2):
+        while (mat[x][y] != 1):
             x = random.randrange(0,len(mat))
             y = random.randrange(0,len(mat[0]))
 
         mat[x][y] = -1
+
+    return mat
+
+def agrega_monedas(mat, n):
+
+    for i in range(n):
+
+        x = random.randrange(0,len(mat))
+        y = random.randrange(0,len(mat[0]))
+
+        while (mat[x][y] < 1):
+            x = random.randrange(0,len(mat))
+            y = random.randrange(0,len(mat[0]))
+
+        mat[x][y] = -2
 
     return mat
 
@@ -98,8 +117,10 @@ def main():
     ancho_cuadrado = 50  
     alto_cuadrado = 50
     cantidad_obstaculos = 10
+    cantidad_monedas = 30
 
     matrix = agrega_obstaculos(matrix, cantidad_obstaculos)
+    matrix = agrega_monedas(matrix, cantidad_monedas)
     #-------------------------------------------------
 
     pygame.display.flip()
